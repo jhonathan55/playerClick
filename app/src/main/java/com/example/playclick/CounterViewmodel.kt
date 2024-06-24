0ec8f887be270c2a9c7022f5e74997b39a7c0d9c
jhonathan55 <jfgutierrezsaico@gmail.com>
2024-06-24 19:55:17 -0400

    CounterViewModel

---
+++ app/src/main/java/com/example/playclick/CounterViewmodel.kt
@@ -0,0 +1,48 @@
+package com.example.playclick
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CounterViewmodel: ViewModel(){
+    private val _counter1 = MutableLiveData(10)
+    val counter1 = _counter1
+
+    private val _counter2 = MutableLiveData(10)
+    val counter2 = _counter2
+
+    private val _winner = MutableLiveData<String?>(null)
+    val winner = _winner
+
+    private fun checkWinner() {
+        if (_counter1.value == 20) {
+            _winner.value = "Player 1"
+        } else if (_counter2.value == 20) {
+            _winner.value = "Player 2"
+        }
+    }
+
+    fun incrementCounter1() {
+        _counter1.value = (_counter1.value ?: 10) + 1
+        checkWinner()
+    }
+
+    fun incrementCounter2() {
+        _counter2.value = (_counter2.value ?: 10) + 1
+        checkWinner()
+    }
+
+    fun decrementCounter1() {
+        _counter1.value = (_counter1.value ?: 10) - 1
+    }
+
+    fun decrementCounter2() {
+        _counter2.value = (_counter2.value ?: 10) - 1
+    }
+
+    fun resetCounters() {
+        _counter1.value = 10
+        _counter2.value = 10
+        _winner.value = null
+    }
+
+}
